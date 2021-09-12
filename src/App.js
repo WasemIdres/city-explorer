569099157ad637f557a55cbe18f8498077f7c03b
WasemIdres <thewasem7915@gmail.com>
2021-09-12 17:46:36 +0300

    update1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Location from './components/Location';
+import SearchForm from './components/SearchForm';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city_name: "",
+      type: "",
+      lat: "",
+      lon: "",
+      showData: false
+    }
+  }
+  handleLocation = (e) => {
+    let city_name = e.target.value;
+    this.setState({
+      city_name: city_name
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let config = {
+      method: "GET",
+      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}`
+    }
+    axios(config).then(res => {
+      console.log(res.data)
+      let reponseData=res.data[0];
+      this.setState({
+        city_name:reponseData.display_name,
+        lon: reponseData.lon,
+        lat: reponseData.lat,
+        showData:true, 
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to City explorer</h1>
+        <SearchForm handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
+        {
+          this.state.showData &&
+          <Location city_name={this.state.city_name}
+            type={this.state.type}
+            lat={this.state.lat}
+            lon={this.state.lon}
+          />
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Location.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import {
+    Table,
+}from 'react-bootstrap'
+class Location extends Component {
+    render() {
+        return (
+            <div style={{marginTop:"20px"}}>
+                {/* <h1>{this.props.city_name}</h1>
+                <h2>{this.props.type}</h2>
+                <h3>{this.props.lat}/{this.props.lon}</h3> */}
+                <Table striped bordered hover variant="dark" style={{width:"750px"}}>
+                    <thead>
+                        <tr>
+                            <th>City Name</th>
+                            <th>latitude</th>
+                            <th>longitude</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{this.props.city_name}</td>
+                            <td>{this.props.lat}</td>
+                            <td>{this.props.lon}</td>
+                        </tr>
+                        
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
+}
+
+export default Location
